e75561dd5b1b6b31befb721bf1aeddee1c76790a
gituser <test@test.de>
2023-11-25 13:39:30 +0100

    SCRUM-4 (Mongo DB Setup): Setup Mongo DB, added 2 Constants MongoDB-Login with google signin: mobileapps11111@gmail.com  :: MobileApps_2023

--- app/src/main/java/com/sem/nutrix/util/Constants.kt
+++ app/src/main/java/com/sem/nutrix/util/Constants.kt
@@ -1,4 +1,7 @@
 package com.sem.nutrix.util
 
 object Constants {
+
+    const val APP_ID = "application-0-mttpy"
+    const val CLIENT_ID = "175284321206-40etj777d5dvkqehjfcje11ries3itf0.apps.googleusercontent.com"
 }
